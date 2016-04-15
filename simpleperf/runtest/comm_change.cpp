7d6c8abc4bdc48ff44db83103f274d9b0bcecc87
Chih-Hung Hsieh <chh@google.com>
2016-04-15 16:12:03 -0700

    Fix some google-runtime-int warnings.
    Bug: 28220065
    Change-Id: Ic6b24216d216bbb165a1420b52b11c7703b810a0

--- simpleperf/runtest/comm_change.cpp
+++ simpleperf/runtest/comm_change.cpp
@@ -8,9 +8,9 @@ void Function1() {
 }
 
 int main() {
-  prctl(PR_SET_NAME, reinterpret_cast<unsigned long>("RUN_COMM1"), 0, 0, 0);
+  prctl(PR_SET_NAME, reinterpret_cast<unsigned long>("RUN_COMM1"), 0, 0, 0); // NOLINT
   Function1();
-  prctl(PR_SET_NAME, reinterpret_cast<unsigned long>("RUN_COMM2"), 0, 0, 0);
+  prctl(PR_SET_NAME, reinterpret_cast<unsigned long>("RUN_COMM2"), 0, 0, 0); // NOLINT
   Function1();
   return 0;
 }
