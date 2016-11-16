3f6ea671d55b0f8ba9bab8826c817327d67ee9bb
Adrien Schildknecht <adriens@google.com>
2016-11-15 22:33:29 -0800

    ext4_utils: use mke2fs instead of make_ext4
    Test: m -j32 && fastboot flashall (aosp_angler-userdebug)
    
    Change-Id: Ic4f598fdf19c8d6fb44740999663ed4f166b0a5b

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -190,4 +190,16 @@ LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))
 LOCAL_IS_HOST_MODULE := true
 include $(BUILD_PREBUILT)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mkuserimg_mke2fs.sh
+LOCAL_SRC_FILES := mkuserimg_mke2fs.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_REQUIRED_MODULES := mke2fs e2fsdroid
+# We don't need any additional suffix.
+LOCAL_MODULE_SUFFIX :=
+LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))
+LOCAL_IS_HOST_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
