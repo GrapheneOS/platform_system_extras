52190a377feeb1dccaae5034c51f0130c24dea09
Yabin Cui <yabinc@google.com>
2017-06-02 14:47:33 -0700

    simpleperf: add thread name in report_sample.proto.
    Bug: http://b/62189080
    Test: run simpleperf_unit_test.
    Change-Id: I3c96f6474e56b2f51a00f85082973e5d1c1aec72

--- simpleperf/cmd_report_sample_test.cpp
+++ simpleperf/cmd_report_sample_test.cpp
@@ -79,3 +79,15 @@ TEST(cmd_report_sample, sample_has_event_count) {
   ASSERT_TRUE(android::base::ReadFileToString(tmpfile2.path, &data));
   ASSERT_NE(data.find("event_count:"), std::string::npos);
 }
+
+TEST(cmd_report_sample, has_thread_record) {
+  TemporaryFile tmpfile;
+  TemporaryFile tmpfile2;
+  ASSERT_TRUE(ReportSampleCmd()->Run({"-i", GetTestData(PERF_DATA_WITH_SYMBOLS),
+                                      "-o", tmpfile.path, "--protobuf"}));
+  ASSERT_TRUE(ReportSampleCmd()->Run(
+      {"--dump-protobuf-report", tmpfile.path, "-o", tmpfile2.path}));
+  std::string data;
+  ASSERT_TRUE(android::base::ReadFileToString(tmpfile2.path, &data));
+  ASSERT_NE(data.find("thread:"), std::string::npos);
+}
