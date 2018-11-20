94da71a79b2551da351fa1a03e82395a5877b3af
Dan Willemsen <dwillemsen@google.com>
2018-11-19 18:32:02 -0800

    Convert blk_alloc_to_base_fs to Android.bp
    See build/soong/README.md for more information.
    
    Also remove a useless HOST_OS check -- HOST_OS will always be either
    linux or darwin, we don't support building directly on windows.
    
    Test: m blk_alloc_to_base_fs
    Change-Id: I13927de82ff769cf3692b58b9432fb79c856b8d3

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -3,23 +3,8 @@
 LOCAL_PATH:= $(call my-dir)
 
 #
-# -- All host/targets including windows
-#
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := blk_alloc_to_base_fs.c
-LOCAL_MODULE := blk_alloc_to_base_fs
-LOCAL_SHARED_LIBRARIES := libcutils
-LOCAL_CFLAGS_darwin := -DHOST
-LOCAL_CFLAGS_linux := -DHOST
-LOCAL_CFLAGS += -Wall -Werror
-include $(BUILD_HOST_EXECUTABLE)
-
-#
 # -- All host/targets excluding windows
 #
-
-ifneq ($(HOST_OS),windows)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mke2fs.conf
@@ -27,5 +12,3 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_IS_HOST_MODULE := true
 include $(BUILD_PREBUILT)
-
-endif
