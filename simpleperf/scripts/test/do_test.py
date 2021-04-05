9662a4c921e6bb2ad3be38a9d89cb88be86d1719
Yabin Cui <yabinc@google.com>
2021-04-05 11:44:21 -0700

    simpleperf: prepare for merging test_monitor.py.
    1. Refactor TestHelper class to remove global variable and add explicit
    init class method.
    2. Merge TestLogger into TestHelper.
    3. Move build_testdata() into do_test.py.
    4. Move test filter into an explicit function.
    
    Bug: 182507493
    Test: run test.py.
    Change-Id: Ia76baa17197f6134ec4d7868e764edb66d29b6bd

--- simpleperf/scripts/test/do_test.py
+++ simpleperf/scripts/test/do_test.py
@@ -14,34 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""test.py: Tests for simpleperf python scripts.
+"""Release test for simpleperf prebuilts.
 
-These are smoke tests Using examples to run python scripts.
-For each example, we go through the steps of running each python script.
-Examples are collected from simpleperf/demo, which includes:
-  SimpleperfExamplePureJava
-  SimpleperfExampleWithNative
-  SimpleperfExampleOfKotlin
-
-Tested python scripts include:
-  app_profiler.py
-  report.py
-  annotate.py
-  report_sample.py
-  pprof_proto_generator.py
-  report_html.py
-
-Test using both `adb root` and `adb unroot`.
+It includes below tests:
+1. Test profiling Android apps on different Android versions (starting from Android N).
+2. Test simpleperf python scripts on different Hosts (linux, darwin and windows) on x86_64.
+3. Test using both devices and emulators.
+4. Test using both `adb root` and `adb unroot`.
 
 """
+
 import argparse
 import fnmatch
 import inspect
+import os
+from pathlib import Path
 import re
-from simpleperf_utils import extant_dir, log_exit, remove
 import sys
+import time
 import types
+from typing import List, Optional
 import unittest
+
+from simpleperf_utils import extant_dir, log_exit, remove, ArgParseFormatter
 
 from . api_profiler_test import *
 from . app_profiler_test import *
@@ -56,10 +51,22 @@ from . report_html_test import *
 from . report_lib_test import *
 from . run_simpleperf_on_device_test import *
 from . tools_test import *
-from . test_utils import TEST_LOGGER, TEST_HELPER
+from . test_utils import TestHelper
 
 
-def get_all_tests():
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgParseFormatter)
+    parser.add_argument('--browser', action='store_true', help='open report html file in browser.')
+    parser.add_argument('--list-tests', action='store_true', help='List all tests.')
+    parser.add_argument('--ndk-path', type=extant_dir, help='Set the path of a ndk release')
+    parser.add_argument('-p', '--pattern', nargs='+',
+                        help='Run tests matching the selected pattern.')
+    parser.add_argument('--test-from', help='Run tests following the selected test.')
+    parser.add_argument('--test-dir', default='test_dir', help='Directory to store test results')
+    return parser.parse_args()
+
+
+def get_all_tests() -> List[str]:
     tests = []
     for name, value in globals().items():
         if isinstance(value, type) and issubclass(value, unittest.TestCase):
@@ -70,59 +77,75 @@ def get_all_tests():
     return sorted(tests)
 
 
-def run_tests(tests):
-    TEST_HELPER.build_testdata()
-    argv = [sys.argv[0]] + tests
-    test_runner = unittest.TextTestRunner(stream=TEST_LOGGER, verbosity=0)
-    test_program = unittest.main(argv=argv, testRunner=test_runner,
-                                 exit=False, verbosity=0, module='test.do_test')
-    result = test_program.result.wasSuccessful()
-    remove(TEST_HELPER.testdata_dir)
-    return result
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Test simpleperf scripts')
-    parser.add_argument('--list-tests', action='store_true', help='List all tests.')
-    parser.add_argument('--test-from', nargs=1, help='Run left tests from the selected test.')
-    parser.add_argument('--browser', action='store_true', help='pop report html file in browser.')
-    parser.add_argument('--progress-file', help='write test progress file')
-    parser.add_argument('--ndk-path', type=extant_dir, help='Set the path of a ndk release')
-    parser.add_argument('pattern', nargs='*', help='Run tests matching the selected pattern.')
-    args = parser.parse_args()
+def get_filtered_tests(test_from: Optional[str], test_pattern: Optional[List[str]]) -> List[str]:
     tests = get_all_tests()
-    if args.list_tests:
-        print('\n'.join(tests))
-        return True
-    if args.test_from:
-        start_pos = 0
-        while start_pos < len(tests) and tests[start_pos] != args.test_from[0]:
-            start_pos += 1
-        if start_pos == len(tests):
-            log_exit("Can't find test %s" % args.test_from[0])
-        tests = tests[start_pos:]
-    if args.pattern:
-        patterns = [re.compile(fnmatch.translate(x)) for x in args.pattern]
+    if test_from:
+        try:
+            tests = tests[tests.index(test_from):]
+        except ValueError:
+            log_exit("Can't find test %s" % test_from)
+    if test_pattern:
+        patterns = [re.compile(fnmatch.translate(x)) for x in test_pattern]
         tests = [t for t in tests if any(pattern.match(t) for pattern in patterns)]
         if not tests:
             log_exit('No tests are matched.')
+    return tests
 
-    if TEST_HELPER.android_version < 7:
-        print("Skip tests on Android version < N.", file=TEST_LOGGER)
-        return False
 
-    TEST_HELPER.ndk_path = args.ndk_path
+def build_testdata(testdata_dir: Path):
+    """ Collect testdata in testdata_dir.
+        In system/extras/simpleperf/scripts, testdata comes from:
+            <script_dir>/../testdata, <script_dir>/test/script_testdata, <script_dir>/../demo
+        In prebuilts/simpleperf, testdata comes from:
+            <script_dir>/test/testdata
+    """
+    testdata_dir.mkdir()
 
-    remove(TEST_HELPER.test_base_dir)
+    script_test_dir = Path(__file__).resolve().parent
+    script_dir = script_test_dir.parent
 
-    if not args.browser:
-        TEST_HELPER.browser_option = ['--no_browser']
+    source_dirs = [
+        script_test_dir / 'script_testdata',
+        script_test_dir / 'testdata',
+        script_dir.parent / 'testdata',
+        script_dir.parent / 'demo',
+    ]
 
-    if args.progress_file:
-        TEST_HELPER.progress_fh = open(args.progress_file, 'w')
+    for source_dir in source_dirs:
+        if not source_dir.is_dir():
+            continue
+        for src_path in source_dir.iterdir():
+            dest_path = testdata_dir / src_path.name
+            if dest_path.exists():
+                continue
+            if src_path.is_file():
+                shutil.copyfile(src_path, dest_path)
+            elif src_path.is_dir():
+                shutil.copytree(src_path, dest_path)
 
-    result = run_tests(tests)
-    if not result:
-        print('Tests failed, see %s for details.' % TEST_LOGGER.log_file, file=TEST_LOGGER)
-    TEST_HELPER.write_progress('Test end')
-    return result
+
+def run_tests(tests: List[str]) -> bool:
+    argv = [sys.argv[0]] + tests
+    test_runner = unittest.TextTestRunner(stream=TestHelper.log_fh, verbosity=0)
+    test_program = unittest.main(argv=argv, testRunner=test_runner,
+                                 exit=False, verbosity=0, module='test.do_test')
+    return test_program.result.wasSuccessful()
+
+
+def main() -> bool:
+    args = get_args()
+    if args.list_tests:
+        print('\n'.join(get_all_tests()))
+        return True
+
+    tests = get_filtered_tests(args.test_from, args.pattern)
+
+    test_dir = Path(args.test_dir).resolve()
+    remove(test_dir)
+    test_dir.mkdir(parents=True)
+    # Switch to the test dir.
+    os.chdir(test_dir)
+    build_testdata(Path('testdata'))
+
+    TestHelper.init('.', 'testdata', args.browser, args.ndk_path, None, None)
+    return run_tests(tests)
