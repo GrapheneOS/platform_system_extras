d93a764818ed6cdb7a112bb2258968934e51c9d0
Yabin Cui <yabinc@google.com>
2023-05-01 16:30:36 -0700

    simpleperf: Add --binary in record cmd
    Copy the --binary option from inject cmd. So we can
    filter branch lists based on binary names while recording.
    
    Bug: 279094308
    Test: run simpleperf_unit_test
    (cherry picked from https://android-review.googlesource.com/q/commit:7e61c625f1a1d906030602c82c1bdaab2370c7af)
    
    Merged-In: Id3e2b092d5748f3d5f8e2b4c4b544e37516ea7eb
    Change-Id: Id3e2b092d5748f3d5f8e2b4c4b544e37516ea7eb

--- simpleperf/cmd_record_test.cpp
+++ simpleperf/cmd_record_test.cpp
@@ -1065,6 +1065,14 @@ TEST(record_cmd, decode_etm_option) {
   ASSERT_TRUE(RunRecordCmd({"-e", "cs-etm", "--decode-etm", "--exclude-perf"}));
 }
 
+TEST(record_cmd, binary_option) {
+  if (!ETMRecorder::GetInstance().CheckEtmSupport().ok()) {
+    GTEST_LOG_(INFO) << "Omit this test since etm isn't supported on this device";
+    return;
+  }
+  ASSERT_TRUE(RunRecordCmd({"-e", "cs-etm", "--decode-etm", "--binary", ".*"}));
+}
+
 TEST(record_cmd, pmu_event_option) {
   TEST_REQUIRE_PMU_COUNTER();
   TEST_REQUIRE_HW_COUNTER();
