28392b53237a6615b2b9609a534b8dce37609a5c
Edgar Arriaga <edgararriaga@google.com>
2023-09-12 18:47:31 +0000

    PinTool add support contextual information and filtering
    Adding support for inspecting zip entries within a zip file
    and providing per-file memory information for them. Also added
    support for filtering based on a pinconfig file to further constrain
    probes or to only generate pinlist.meta files for a certain set of
    files within a zip file.
    
    Test: meminspect_tests
    Test: pintool_tests
    Bug: 297095632
    
    Change-Id: Ie1a75f44606c4cd7a0971a09322169c01bdca325

--- pinner/pintool.cpp
+++ pinner/pintool.cpp
@@ -1,4 +1,4 @@
-#include <android-base/stringprintf.h>
+#include <android-base/parseint.h>
 #include <fcntl.h>
 #include <sys/endian.h>
 #include <sys/mman.h>
@@ -15,27 +15,82 @@
 using namespace std;
 using namespace android::base;
 
-enum ToolMode { PROBE, DUMP, UNKNOWN };
+enum ToolMode {
+    MAPPED_FILE,  // Files that are mapped in memory
+    PINLIST,      // pinlist.meta style file
+    UNKNOWN
+};
 
-void print_pinner_ranges(const std::vector<VmaRange>& ranges) {
-    cout << "vmas to pin:" << endl;
-    for (int i = 0; i < ranges.size(); ++i) {
-        cout << StringPrintf("start=%u length=%u", ranges[i].offset, ranges[i].length) << endl;
+void print_pinlist_ranges(const std::vector<VmaRange>& ranges) {
+    cout << "--pinlist memory ranges--" << endl;
+    for (auto&& range : ranges) {
+        cout << "start=" << range.offset << " bytes=" << range.length << endl;
     }
 }
 
-int perform_probe(const vector<string>& options) {
-    std::string probed_file;
-    std::string output_file = "/data/local/tmp/pinlist.meta";
-    bool verbose = false;
-    int pages_per_mincore = DEFAULT_PAGES_PER_MINCORE;
+void print_pinlist_summary(const std::vector<VmaRange>& ranges) {
+    cout << "--pinlist summary--" << endl;
+    uint64_t total_bytes = 0;
+    for (auto&& range : ranges) {
+        total_bytes += range.length;
+    }
+    cout << "total_bytes_to_pin=" << total_bytes << endl;
+}
 
-    // Parse Args
-    for (int i = 0; i < options.size(); ++i) {
+int perform_file_action(const vector<string>& options) {
+    std::string custom_probe_file;
+    std::string output_file;
+    std::string pinconfig_file;
+
+    bool verbose = false;
+    bool is_zip = false;
+    bool dump_results = false;
+    ProbeType probe_type = UNSET;
+    int64_t write_quota = -1;  // unbounded by default
+
+    if (options.empty()) {
+        cerr << "Missing filename for file action, see usage for details." << endl;
+        return 1;
+    }
+
+    std::string input_file = options[0];
+
+    // Validate that the file exists.
+    if (get_file_size(input_file) == -1) {
+        cerr << "Error: Could not read file: " << input_file << endl;
+        return 1;
+    }
+
+    if (input_file.empty()) {
+        cerr << "Error: Should specify an input file." << endl;
+        return 1;
+    }
+
+    // Parse flags
+    for (int i = 1; i < options.size(); ++i) {
         string option = options[i];
-        if (option == "-p") {
+        if (option == "--gen-probe") {
+            if (probe_type != ProbeType::UNSET) {
+                cerr << "Should only specify one probe treatment. See usage for details." << endl;
+                return 1;
+            }
+            probe_type = ProbeType::GENERATE;
+            continue;
+        }
+
+        if (option == "--use-probe") {
+            if (probe_type != ProbeType::UNSET) {
+                cerr << "Should only specify one probe treatment. See usage for details." << endl;
+                return 1;
+            }
+            probe_type = ProbeType::CUSTOM;
             ++i;
-            probed_file = options[i];
+            custom_probe_file = options[i];
+            continue;
+        }
+        if (option == "--pinconfig") {
+            ++i;
+            pinconfig_file = options[i];
             continue;
         }
         if (option == "-o") {
@@ -43,125 +98,204 @@ int perform_probe(const vector<string>& options) {
             output_file = options[i];
             continue;
         }
+        if (option == "--quota") {
+            ++i;
+            android::base::ParseInt(options[i], &write_quota);
+            continue;
+        }
         if (option == "-v") {
             verbose = true;
             continue;
         }
-        if (option == "-w") {
-            ++i;
-            pages_per_mincore = stoi(options[i]);
+        if (option == "--zip") {
+            is_zip = true;
+            continue;
+        }
+        if (option == "--dump") {
+            dump_results = true;
             continue;
         }
     }
 
     if (verbose) {
-        cout << "mincore window size=" << pages_per_mincore << endl;
-        cout << "Setting output pinlist file as " << probed_file.c_str() << endl;
-        cout << "Setting file to probe: " << probed_file.c_str() << endl;
+        cout << "Setting output pinlist file: " << output_file.c_str() << endl;
+        cout << "Setting input file: " << input_file.c_str() << endl;
+        cout << "Setting pinconfig file: " << pinconfig_file.c_str() << endl;
+        cout << "Setting custom probe file: " << custom_probe_file.c_str() << endl;
+        cout << "Setting probe type: " << probe_type << endl;
+        cout << "Dump enabled: " << dump_results << endl;
+        cout << "Is Zip file: " << is_zip << endl;
+        if (write_quota != -1) {
+            cout << "Set Write quota: " << write_quota << endl;
+        }
     }
 
-    if (probed_file.empty()) {
-        cerr << "Error: Should specify a file to probe.";
-        return 1;
-    }
+    PinTool pintool(input_file);
 
-    ResidentMemResult memresult;
-    int res = probe_resident_memory(probed_file, memresult, pages_per_mincore);
-    if (res) {
-        if (res == MEMINSPECT_FAIL_OPEN) {
-            cerr << "Failed to open file: " << probed_file << endl;
+    if (is_zip) {
+        pintool.set_verbose_output(verbose);
+        if (probe_type == ProbeType::CUSTOM) {
+            if (verbose) {
+                cout << "Using custom probe file: " << custom_probe_file << endl;
+            }
+            pintool.read_probe_from_pinlist(custom_probe_file);
+        } else if (probe_type == ProbeType::GENERATE) {
+            if (verbose) {
+                cout << "Generating probe" << endl;
+            }
+            int res = pintool.probe_resident();
+            if (res > 0) {
+                cerr << "Failed to generate probe. Error Code: " << res << endl;
+                return 1;
+            }
+        }
+        pintool.compute_zip_entry_coverages();
+
+        if (pinconfig_file.length() > 0) {
+            // We have provided a pinconfig file so perform filtering
+            // of computed coverages based on it.
+            pintool.filter_zip_entry_coverages(pinconfig_file);
         }
 
-        if (res == MEMINSPECT_FAIL_FSTAT) {
-            cerr << "Failed to fstat file: " << probed_file << endl;
+        if (dump_results) {
+            cout << endl << "----Unfiltered file coverages----" << endl << endl;
+            pintool.dump_coverages(PinTool::DumpType::FILE_COVERAGE);
+
+            if (pinconfig_file.length() > 0) {
+                cout << endl << "----Filtered file coverages----" << endl << endl;
+                pintool.dump_coverages(PinTool::DumpType::FILTERED);
+            }
         }
 
-        if (res == MEMINSPECT_FAIL_MINCORE) {
-            cerr << "Mincore failed for file: " << probed_file << endl;
+        if (output_file.length() > 0) {
+            pintool.write_coverages_as_pinlist(output_file, write_quota);
         }
 
+        return 0;
+    } else {
+        if (probe_type != ProbeType::GENERATE) {
+            cerr << "Only generating probes is supported for non-zip files, please include "
+                    "--gen-probe on your command"
+                 << endl;
+            return 1;
+        }
+
+        // Generic file probing will just return resident memory and offsets
+        // without more contextual information.
+        VmaRangeGroup resident;
+
+        int res = pintool.probe_resident();
+        if (res > 0) {
+            cerr << "Failed to generate probe. Error Code: " << res << endl;
+            return 1;
+        }
+
+        pintool.dump_coverages(PinTool::DumpType::PROBE);
+
+        if (output_file.length() > 0) {
+            res = write_pinlist_file(output_file, resident.ranges, write_quota);
+            if (res > 0) {
+                cerr << "Failed to write pin file at: " << output_file << endl;
+            } else if (verbose) {
+                cout << "Finished writing pin file at: " << output_file << endl;
+            }
+        }
         return res;
     }
-
-    cout << StringPrintf(
-                    "Finished Probing. resident memory(KB)=%llu. file_size (KB)=%llu. "
-                    "pin_percentage=%f",
-                    (unsigned long long) (memresult.total_resident_bytes / 1024), (unsigned long long) (memresult.file_size_bytes / 1024),
-                    memresult.total_resident_bytes / (float)memresult.file_size_bytes * 100)
-         << endl;
-
-    res = write_pinlist_file(output_file, memresult.resident_memory_ranges);
-    if (res > 0) {
-        cerr << "Failed to write pin file at: " << output_file << endl;
-    } else {
-        if (verbose) {
-            cout << "Finished writing pin file at: " << output_file << endl;
-        }
-    }
-    return res;
+    return 0;
 }
 
-int perform_dump(const vector<string>& options) {
+int perform_pinlist_action(const vector<string>& options) {
     string pinner_file;
     bool verbose = false;
-    for (int i = 0; i < options.size(); ++i) {
+    bool dump = false;
+    bool summary = false;
+
+    if (options.size() < 1) {
+        cerr << "Missing arguments for pinlist mode. See usage for details << endl";
+        return 1;
+    }
+    pinner_file = options[0];
+    for (int i = 1; i < options.size(); ++i) {
         string option = options[i];
-        if (option == "-p") {
-            ++i;
-            pinner_file = options[i];
-            continue;
-        }
 
         if (option == "-v") {
             verbose = true;
         }
+
+        if (option == "--dump") {
+            dump = true;
+        }
+
+        if (option == "--summary") {
+            summary = true;
+        }
     }
+
     if (pinner_file.empty()) {
         cerr << "Error: Pinlist file to dump is missing. Specify it with '-p <file>'" << endl;
         return 1;
     }
+
     if (verbose) {
         cout << "Setting file to dump: " << pinner_file.c_str() << endl;
     }
+
     vector<VmaRange> vma_ranges;
     if (read_pinlist_file(pinner_file, vma_ranges) == 1) {
         cerr << "Failed reading pinlist file" << endl;
     }
-    print_pinner_ranges(vma_ranges);
+
+    if (dump) {
+        print_pinlist_ranges(vma_ranges);
+    }
+
+    if (summary) {
+        print_pinlist_summary(vma_ranges);
+    }
 
     return 0;
 }
 
+void print_usage() {
+    const string usage = R"(
+    Expected usage: pintool <mode> <required> [option]
+    where:
+    ./pintool <MODE>
+    <MODE>
+        file <filename> [option]
+            [option]
+                --gen-probe
+                    Generate a probe from current resident memory based on provided "file"
+                --use-probe <path_to_input_pinlist.meta>
+                    Use a previously generated pinlist.meta style file as the probe to match against.
+                --dump
+                    Dump output contents to console.
+                --zip
+                    Treat the file as a zip/apk file required for doing per-file coverage analysis and generation.
+                --pinconfig <path_to_pinconfig.txt>
+                    Filter output coverage ranges using a provided pinconfig.txt style file. See README.md for samples
+                    on the format of that file.
+                -v
+                    Enable verbose output.
+
+        pinlist <pinlist_file> [option]
+            <pinlist_file>
+                this is the file that will be used for reading and it should follow the pinlist.meta format.
+            [option]
+                --dump
+                    Dump <pinlist_file> contents to console output.
+                -v
+                    Enable verbose output.
+                --summary
+                    Summary results for the pinlist.meta file
+    )";
+    cout << usage.c_str();
+}
+
 int main(int argc, char** argv) {
     if (argc == 1) {
-        const string usage = R"(
-Expected usage: pintool <mode> <required> [option]
-where:
-<file_to_pin> is a file currently mapped by another process and in memory.
-<mode> :
-    probe
-        This mode will probe resident memory for a file and generate a pinlist.meta file
-        that can be interpreted by the PinnerService.
-
-        <required>
-            -p <file_to_probe>
-                This option will probe the specified file
-        [option]:
-            -o <file>
-                Specify the output file for the pinlist file.
-                (default=/data/local/tmp/pinlist.meta)
-            -v
-                Enable verbose output.
-            -w
-                Mincore total pages per mincore window. Bigger windows
-                will use more memory but may be slightly faster. (default=1)
-    dump
-        <required>
-            -p <input_pinlist_file>
-                Specify the input pinlist file to dump
-)";
-
-        cout << usage.c_str();
+        print_usage();
         return 0;
     }
 
@@ -169,14 +303,20 @@ where:
         cerr << "<mode> is missing";
         return 1;
     }
-    ToolMode mode = UNKNOWN;
-    if (strcmp(argv[1], "probe") == 0) {
-        mode = PROBE;
-    } else if (strcmp(argv[1], "dump") == 0) {
-        mode = DUMP;
+
+    if (strcmp(argv[1], "--help") == 0) {
+        print_usage();
+        return 0;
     }
 
-    if (mode == UNKNOWN) {
+    ToolMode mode = ToolMode::UNKNOWN;
+    if (strcmp(argv[1], "file") == 0) {
+        mode = ToolMode::MAPPED_FILE;
+    } else if (strcmp(argv[1], "pinlist") == 0) {
+        mode = ToolMode::PINLIST;
+    }
+
+    if (mode == ToolMode::UNKNOWN) {
         cerr << "Failed to find mode: " << argv[1] << ". See usage for available modes." << endl;
         return 1;
     }
@@ -188,15 +328,15 @@ where:
 
     int res;
     switch (mode) {
-        case PROBE:
-            res = perform_probe(options);
+        case ToolMode::MAPPED_FILE:
+            res = perform_file_action(options);
             break;
-        case DUMP:
-            res = perform_dump(options);
+        case ToolMode::PINLIST:
+            res = perform_pinlist_action(options);
             break;
-        case UNKNOWN:
+        case ToolMode::UNKNOWN:
+            cerr << "Unknown <MODE> see usage for details." << endl;
             return 1;
-            break;
     }
 
     return res;
